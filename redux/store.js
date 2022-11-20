1e2e714ff84e14846fe83daf50b55adadb65d6ec
cepedagm14 <cepedagm14@gmail.com>
2022-11-20 01:05:10 -0300

    add middleware

--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import Reducer from "./reducer";
-
+import listenerMiddleware from "./middlewareListener";
 
 export const store = configureStore({
   reducer: {
     app: Reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(listenerMiddleware.middleware),
 });
